7b394ed13dc12c00909d7a09fa53f4e95fd1b60a
Ravi-047 <ravi047raj@gmail.com>
2023-05-25 18:23:38 +0530

    mincss

--- src/Components/MainLayout.jsx
+++ src/Components/MainLayout.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 
+// importing icon
+import { AiOutlineMenuFold } from "react-icons/ai";
+
+import "./mainLayout.css";
+
 const MainLayout = () => {
+  const [isSidebarClosed, setIsSidebarClosed] = useState(false);
+  const toggleSidebar = () => {
+    setIsSidebarClosed(!isSidebarClosed);
+  };
   return (
-    <div>
-      <h3>This is main Layout</h3>
-      <Outlet />
-    </div>
+    <>
+      <div className={`sidebar ${isSidebarClosed ? "close" : ""}`}>
+        <p>side</p>
+        <div className="side_togge_menu">
+          <AiOutlineMenuFold />
+        </div>
+      </div>
+      <div className="home_section">
+        <Outlet />
+      </div>
+    </>
   );
 };
 
